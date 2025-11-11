f5eeb20386abfab2fb667bd2e8a3b27d5dfcb2f7
Kevin Chavarin Pineda <Kevin.chavarin@alumnos.udg.mx>
2025-11-11 15:53:32 -0600

    Adding the navbar and header content along with styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
+import '../styles/Header.css';
+import image from '../assets/restauranfood.jpg';
 
 function Header() {
   return (
     <header>
-      <h1>Welcome to My Website</h1>
+      <div className='header-container'>
+        <h1 className='ll-title'>Little Lemon</h1>
+        <h3 className='header-location'>Chicago</h3>
+        <p className='header-description'>
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+        </p>
+        <button className='reserve-btn'>Reserve a Table</button>
+      </div>
+      <div>
+        <img src={image} className="restaurant-image" alt="Restaurant Food" />
+      </div>
     </header>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
-
+import '../styles/Nav.css';
+import logo from '../assets/logo.svg';
 
 function Nav() {
   return (
     <nav>
-      <img src="../assets/logo.svg" alt="Logo" />
+      <img src={logo} className="logo" alt="Logo" />
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
